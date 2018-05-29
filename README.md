0e09430a8f74540481d4fe38df0a18f72069c849
Raymond Otto <otto@rgpo.nl>
2018-05-29 16:25:06 +0200

    README and run.sh update

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
+# K8s 100% through Terraform
 
+## History
 
+## Good to know
+This repo tries to use branch tagging which match the versions of Kubernetes:
+Kubernetes version | repo version
+* v1.5.7 | v1.5
+* v1.9.7 | v1.9
+
+At the moment of writing all infrastructure is provisioned within the eu-west-1 region. 
+
+## Preparing
+We first need to clone the repo.
+```
+cd /data/github
+git clone https://github.com/digiwhite1980/k8s.git
+```
+After cloning, the shared/aws_credentials.tf needs to be created. Copy the example file from the directory and provide the key and secret credentials.
+```
+cd shared/
+cp -p aws_credentials.tf.example aws_credentials.tf
+```
+
+## Running
+To make things easy a run.sh script is made available which allows the deployment of your kubernetes environment in stages.
+The repo contains numbered folders starting at 01 and ends with 06.
+
+All terraform and kubectl command are done through the default hashicorp/terraform docker container.
+
+```
+docker run -ti --name terraform_k8s -v ~{path to rootfolder of git repo}/:/data/github --entrypoint=/bin/sh hashicorp/terraform
+cd /data/k8s/01_infra/terraform
+terraform init --upgrade
+```
+
+## 
 Download CNI plugins through git:
 https://github.com/containernetworking/plugins
 
--- run.sh
+++ run.sh
@@ -30,37 +30,70 @@ function binCheck {
 function usage {
 cat <<_EOF_
 
-	Usage: ${0} [-h Help] [-e ETCD] [-a API] [-k Kubelet] [-A All] [-D Destroy]"
+	$1
 
-		-h 		This help
+	Usage: ${0} -E <environment> -n <CIDR prefix x.x>
+					[-h Help] [-i Infra] [-e ETCD] [-a API] [-k Kubelet] [-s Services] [-c Custom] [-A All] [-D Destroy]
+
+		-E			Environment (mandatory)
+		-n			CIDR prefix [x.x] <first 2 digits of ipv4 network>
+
+		-i			Run infra
 		-e 		Run ETCD terraform
 		-a 		Run Kubernetes API server terraform
 		-k 		Run Kubernetes Kubelete terraform
+		-s			Run services
+		-c			Custom scripts (if made available)
 		-A 		Run All terraform 
 		-D 		Run destroy terraform 
+		-h 		This help
 _EOF_
+	[[ "${1}" != "" ]] && exit 1
 }
 
-while getopts ":eakhAD" opt; do
+while getopts ":eiaskhcADE:n:" opt; do
 	case $opt in
 		h)
 			usage
 			exit 0
 			;;
+		i)
+			INFRA=1
+			EXEC=1
+			;;
 		e)
 			ETCD=1
+			EXEC=1
 			;;
 		a)
 			KUBEAPI=1
+			EXEC=1
 			;;
 		k)
 			KUBELET=1
+			EXEC=1
+			;;
+		s)
+			SERVICES=1
+			EXEC=1
+			;;
+		c)
+			CUSTOM=1
+			EXEC=1
 			;;
 		D)
 			DESTROY=1
+			EXEC=1
 			;;
 		A)
 			ALL=1
+			EXEC=1
+			;;
+		E)
+			ENVIRONMENT=${OPTARG}
+			;;
+		n)
+			CIDR_PREFIX=${OPTARG}
 			;;
 		?)
 			usage
@@ -69,10 +102,14 @@ while getopts ":eakhAD" opt; do
 	esac
 done
 
+EXEC=${EXEC:-0}
 ALL=${ALL:-0}
+INFRA=${INFRA:-0}
 ETCD=${ETCD:-0}
 KUBEAPI=${KUBEAPI:-0}
 KUBELET=${KUBELET:-0}
+SERVICES=${SERVICES:-0}
+CUSTOM=${CUSTOM:-0}
 DESTROY=${DESTROY:-0}
 
 CURRENT_FOLDER=$(pwd)
@@ -83,40 +120,75 @@ binCheck git terraform
 [[ ! -x $(basename ${0}) ]] 	&& log 3 "Please execute $(basename ${0}) from local directory (./run.sh)"
 
 git submodule init
-[[ $? -ne 0 ]] && log 3 "Failed to initialize submodules"
+[[ $? -ne 0 ]]	 		&& log 3 "Failed to initialize submodules"
 
 git submodule update
-[[ $? -ne 0 ]] && log 3 "Failed to update submodules"
+[[ $? -ne 0 ]] 			&& log 3 "Failed to update submodules"
 
+[[ "${CIDR_PREFIX}" == "" ]] 	&& usage "No CIDR prefix (-n) set"
+[[ "${ENVIRONMENT}" == "" ]] 	&& usage "No environment (-E) set"
+[[ ${EXEC} -ne 1 ]] 		&& usage "No action selected"
 
 ########################################################################################
 
-if [ ${ETCD} -eq 1 -o ${ALL} -eq 1 ]; then
-	[[ ! -d "etcd" ]] 		&& log 3 "Unable to find etcd folder for option -e"
-	cd etcd
-	log 1 "Executing terraform init"
+if [ ${INFRA} -eq 1 -a ${ALL} -ne 1 ]; then
+	[[ ! -d "01_infra" ]] && log 3 "Unable to find infra folder for option -i"
+	cd 01_infra/terraform	
+	log 1 "Executing terraform init on infra"
 	terraform init > /dev/null
-	terraform apply -state .terraform_state
-fi
-
-if [ ${KUBEAPI} -eq 1 -o ${ALL} -eq 1 ]; then
-	[[ ! -d "kubeapi" ]] 	&& log 3 "Unable to find kubeapi folder for option -a"
-	echo KUBEAPI
-fi
-
-if [ ${KUBELET} -eq 1 -o ${ALL} -eq 1 ]; then
-	[[ ! -d "kubelet" ]] 	&& log 3 "Unable to find kubelet folder for option -k"
-	echo KUBELET
+	terraform apply -var env=${ENVIRONMENT} -var cidr_prefix=${CIDR_PREFIX}
+	cd -
 fi
 
 
+if [ ${ETCD} -eq 1 -a ${ALL} -ne 1 ]; then
+	[[ ! -d "02_etcd" ]] && log 3 "Unable to find etcd folder for option -e"
+	cd 02_etcd/terraform
+	log 1 "Executing terraform init etcd"
+	terraform init > /dev/null
+	terraform apply -var env=${ENVIRONMENT} -var cidr_prefix=${CIDR_PREFIX}
+	cd -
+fi
 
+if [ ${KUBEAPI} -eq 1 -a ${ALL} -ne 1 ]; then
+	[[ ! -d "03_kubeapi" ]] && log 3 "Unable to find kubeapi folder for option -a"
+	cd 03_kubeapi/terraform
+	log 1 "Executing terraform init kubeapi"
+	terraform init > /dev/null
+	terraform apply -var env=${ENVIRONMENT} -var cidr_prefix=${CIDR_PREFIX}
+	cd -
+fi
 
+if [ ${KUBELET} -eq 1 -a ${ALL} -ne 1 ]; then
+	[[ ! -d "04_kubelet" ]] && log 3 "Unable to find kubelet folder for option -k"
+	cd 04_kubelet/terraform
+	log 1 "Executing terraform init kubelet"
+	terraform init > /dev/null
+	terraform apply -var env=${ENVIRONMENT} -var cidr_prefix=${CIDR_PREFIX}
+	cd -
+fi
 
-terraform init
-[[ $? -ne 0 ]] && log 3 "Failed to initialize terraform"
+if [ ${SERVICES} -eq 1 -a ${ALL} -ne 1 ]; then
+	[[ ! -d "05_services" ]] && log 3 "Unable to find services folder for option -s"
+	cd 05_services/terraform
+	log 1 "Executing terraform init services"
+	terraform init > /dev/null
+	terraform apply -var env=${ENVIRONMENT} -var cidr_prefix=${CIDR_PREFIX}
+	cd -
+fi
 
-terraform plan -state .terraform_state > /dev/null 2>&1
-[[ $? -ne 0 ]] && log 3 "Failed to plan terraform"
+if [ ${CUSTOM} -eq 1 -o ${ALL} -eq 1 ]; then
+	[[ ! -d "06_custom" ]] && log 3 "Unable to find custom folder for option -s"
+	cd 06_custom/terraform
+	log 1 "Executing terraform init custom"
+	terraform init > /dev/null
+	terraform apply -var env=${ENVIRONMENT} -var cidr_prefix=${CIDR_PREFIX}
+	cd -
+fi
 
-terraform apply -state .terraform_state
+if [ ${DESTROY} -eq 1 ]; then
+	[[ ! -d "01_infra" ]] && log 3 "Unable to find custom folder for option -D"
+	cd 01_infra/terraform
+	log 1 "Executing terraform destroy"
+	terraform destroy -var env=${ENVIRONMENT} -var cidr_prefix=${CIDR_PREFIX}
+fi
